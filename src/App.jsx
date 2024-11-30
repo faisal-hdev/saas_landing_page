86574f3f718795f671baa6689cbc6af484f4338a
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-11-30 23:02:24 +0600

    saas_landing_page repo created and header working are running

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./sections/Header";
+
+function App() {
+  return (
+    <main>
+      <div className="overflow-hidden">
+        <Header />
+      </div>
+    </main>
+  );
+}
+
+export default App;
